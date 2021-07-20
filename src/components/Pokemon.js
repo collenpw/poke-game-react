c4aa8905c6c2fcefac2bd0d21e1f4012e03636fc
collenpw <collenpw13@gmail.com>
2021-07-20 19:00:23 -0400

    Build out basic location finder

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroup';
@@ -6,34 +8,76 @@ import ListGroupItem from 'react-bootstrap/ListGroup';
 
 const Pokemon = ({pokeData}) => {
 
+    const [locations, setLocations] = useState([]);
 
-    console.log(pokeData.moves);
+    const getLocationData = async() => {
+        try {
+            const res = await fetch (`${pokeData.location_area_encounters}`);
+            const data = await res.json();
+            setLocations(data);
+            console.log(locations);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    const capitalize = (str) => {
+        return str.charAt(0).toUpperCase() + str.slice(1)
+    }
+
+    const formatLocation = (str) => {
+        let formattedLoc = str.replace(/-/g, ' ');
+        formattedLoc = formattedLoc.replace(/area/g, '')
+
+        return capitalize(formattedLoc);
+    }
+
+
+    useEffect(() => {
+        getLocationData();
+    },[])
+
+
+    console.log(locations[2]);
+
+
+    console.log(pokeData.location_area_encounters);
 
         return (
+            <>
+                <Card border='dark'style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeData.id}.png`} />
+                    <Card.Body>
+                        <Card.Title>{capitalize(pokeData.name)}</Card.Title>
+                    </Card.Body>
+                    <ListGroup className="abilities">
+                        <ListGroup.Item>Abilities:</ListGroup.Item>
+                        {pokeData.abilities.map((ability) => {
+                            return (
+                                <ListGroup.Item>{capitalize(ability.ability.name)}</ListGroup.Item>
+                            )
+                        })}
+                    </ListGroup>
+                    <ListGroup className="type">
+                        <ListGroup.Item>Types:</ListGroup.Item>
+                        {pokeData.types.map((type) => {
+                            return (
+                                <ListGroup.Item>{capitalize(type.type.name)}</ListGroup.Item>
+                            )
+                        })}
+                    </ListGroup>
+                    
+                </Card>
 
-            <Card border='dark'style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeData.id}.png`} />
-                <Card.Body>
-                    <Card.Title>{pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)}</Card.Title>
-                </Card.Body>
-                <ListGroup className="abilities">
-                    <ListGroupItem>Abilities:</ListGroupItem>
-                    {pokeData.abilities.map((ability) => {
+                <ListGroup className='locations'>
+                    {locations.map((location) => {
                         return (
-                            <ListGroupItem>{ability.ability.name.charAt(0).toUpperCase() + ability.ability.name.slice(1)}</ListGroupItem>
+                            <ListGroup.Item>{`Pokemon: ${capitalize(location.version_details[0].version.name)} -- ${capitalize(location.version_details[0].encounter_details[0].method.name)} at ${formatLocation(location.location_area.name)}`}</ListGroup.Item>
                         )
                     })}
                 </ListGroup>
-                <ListGroup className="type">
-                    <ListGroupItem>Types:</ListGroupItem>
-                    {pokeData.types.map((type) => {
-                        return (
-                            <ListGroupItem>{type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)}</ListGroupItem>
-                        )
-                    })}
-                </ListGroup>
-                
-            </Card>
+            </>
             );
         
   
