ae1d8be1f05989d09eaf461b95dfb8d4c36f20c6
Collen <collenpw13@gmail.com>
2021-08-10 14:12:51 -0400

    Media breakpoints

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -12,48 +12,48 @@ import { DataContext } from "../../App";
 import { useHistory } from "react-router";
 
 
-const Navigation = ({currentPokeUser}) => {
+const Navigation = ({ currentPokeUser }) => {
 
     const data = useContext(DataContext);
     const history = useHistory();
 
     console.log(data);
-  
+
     return (
-        
+
         <Navbar sticky='top' bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand onClick={() => {history.push('/')}} >
-                    <img 
+            <Container className='no-padding'>
+                <Navbar.Brand onClick={() => { history.push('/') }} >
+                    <img
                         src={logo}
                         width="160"
                         height="30"
                         className="d-inline-block align-top nav-img"
-                        alt="okie-dokie-dex logo" 
+                        alt="okie-dokie-dex logo"
                     />
                 </Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link onClick={()=> {history.push('/pokemon')}}>Pokemon</Nav.Link>
-                    <Nav.Link onClick={()=> {history.push('/moves')}}>Moves</Nav.Link>
-                    <Nav.Link onClick={()=> {history.push('/abilities')}}>Abilities</Nav.Link>                       
-                    <Nav.Link onClick={()=> {history.push('/types')}}>Types</Nav.Link>
-                    {data.isAuthenticated && currentPokeUser &&(
+                    <Nav.Link onClick={() => { history.push('/pokemon') }}>Pokemon</Nav.Link>
+                    <Nav.Link onClick={() => { history.push('/moves') }}>Moves</Nav.Link>
+                    <Nav.Link onClick={() => { history.push('/abilities') }}>Abilities</Nav.Link>
+                    <Nav.Link onClick={() => { history.push('/types') }}>Types</Nav.Link>
+                    {data.isAuthenticated && currentPokeUser && (
                         <>
                             <FavPokeButton />
                         </>
                     )}
                 </Nav >
                 <Nav>
-                    {!data.isAuthenticated && (<Login/>)}
-                    {data.isAuthenticated && currentPokeUser &&(
+                    {!data.isAuthenticated && (<Login />)}
+                    {data.isAuthenticated && currentPokeUser && (
                         <>
-                        <Navbar.Text>Logged in as {currentPokeUser.username}:</Navbar.Text>
-                        <Logout currentPokeUser={currentPokeUser} />
+                            <Navbar.Text>Logged in as {currentPokeUser.username}:</Navbar.Text>
+                            <Logout currentPokeUser={currentPokeUser} />
                         </>
                     )}
                 </Nav>
             </Container>
-         </Navbar>
+        </Navbar>
 
     );
 };
